4ab163fe78afb22e772c9bc54a913035409288a0
1 <1@1-PC>
2017-12-25 16:01:39 +0800

    1

---
+++ src/main/java/com/cn/hnust/pojo/PageInfo.java
@@ -0,0 +1,28 @@
+package com.cn.hnust.pojo;
+
+public class PageInfo {
+	private int pageSize;
+	private int pageIndex;
+	private int totalCount;
+	private int pageCount;
+	public int getTotalCount() {
+		return totalCount;
+	}
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+	public int getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+	public int getPageIndex() {
+		return pageIndex;
+	}
+	public void setPageIndex(int pageIndex) {
+		this.pageIndex = pageIndex;
+	}
+	
+
+}
